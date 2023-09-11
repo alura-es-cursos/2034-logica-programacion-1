6c141277927efba320882047e39e5aed159bfa2c
Leonardo Jose <ljcl79@gmail.com>
2023-09-11 08:12:13 -0300

    Aula3

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 //Variables
-let numeroSecreto = 2;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 
-console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    //La condición no se cumplió
-    alert('Lo siento, no acertaste el número');
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+    console.log(numeroUsuario);
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
 }
