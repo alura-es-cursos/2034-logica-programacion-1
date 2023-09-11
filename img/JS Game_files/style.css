3457eed30f4f0a264897dc5252acbe511d534d32
Leonardo Jose <ljcl79@gmail.com>
2023-09-11 08:10:17 -0300

    ProyectoInicial

---
+++ img/JS Game_files/style.css
@@ -0,0 +1,8 @@
+* {
+    background-color: black;
+    background-image: url('./img/bg.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    opacity: 0.4;
+}
