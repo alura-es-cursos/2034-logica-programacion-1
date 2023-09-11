84db8c717dad03375bdec5704d81d4effe1e14fa
Leonardo Jose <ljcl79@gmail.com>
2023-09-11 08:13:13 -0300

    Aula5

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //Variables
-let numeroSecreto = Math.floor(Math.random()*10)+1;
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario = 0;
 let intentos = 1;
 //let palabraVeces = 'vez';
-let maximosIntentos = 3;
+let maximosIntentos = 6;
 
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
 
     console.log(typeof(numeroUsuario));
     if (numeroUsuario == numeroSecreto) {
