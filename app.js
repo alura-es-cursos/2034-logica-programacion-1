295802e0114eeb69617fbde0c5ba9d80ab7ff17b
Leonardo Jose <ljcl79@gmail.com>
2023-09-11 08:11:06 -0300

    Aula1

---
+++ app.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un número por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el número');
+}
