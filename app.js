89578acdf83f39e01b06dd76fd0763c610682d0a
AndrewCarvajal97 <127360563+AndrewCarvajal97@users.noreply.github.com>
2024-01-17 16:14:43 -0500

    comit primera clase
    hacemos un commit de los temas vistos en la primera clase

---
+++ app.js
@@ -0,0 +1,13 @@
+let numeroSecreto = 6 ;
+let numeroUsuario=prompt("ingrese un numero");
+
+//alert("Hola mundo ");
+console.log(numeroUsuario);
+alert(numeroUsuario);
+
+
+if(numeroUsuario == numeroSecreto){
+	alert("acertaste el numero")}
+else{
+	alert("no acertaste el numero" )
+	}
