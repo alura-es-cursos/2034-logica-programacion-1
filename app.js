45886ea9266ae5b1e0d2e2c6f86ae0ed4fe10687
analopbec <139185675+analopbec@users.noreply.github.com>
2024-01-18 23:29:29 +0000

    1

---
+++ app.js
@@ -0,0 +1,19 @@
+alert('Bienvenidos al juego del número secreto');
+
+//Variables
+let numeroSecreto = 2;
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+console.log(numeroUsuario);
+/*
+Este código realiza
+la comparación
+*/
+if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+} else {
+    //La condición no se cumplió
+    alert('Lo siento, no acertaste el número');
+}
+
